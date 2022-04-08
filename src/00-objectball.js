1d53fd8bf9c784920cbdfd13bdecd6c351a79de7
hoonkim <hoonk9387@gmail.com>
2022-04-08 10:16:51 -0500

    Completed assignment

--- src/00-objectball.js
+++ src/00-objectball.js
@@ -1,140 +1,170 @@
 /* #1 numPointsScored */
-// function numPointsScored(playerName) {
-//     // debugger
-//     let game = gameObject()
+/* 
+input: player's name 
+output: return number of points scored for that player
+*/
+function numPointsScored(playerName) {
+    // debugger
+    let game = gameObject()
 
-//     // Look at key home.
-//     // Look at key players.
-//     // Check if players obj has key that equals argument playerName.
-//     // If true return playerName.points.
-//     // Else look at key away.
-//     // Look at key players.
-//     // Check if players obj has key that equals argument playerName.
+    // Look at key home.
+    // Look at key players.
+    // Check if players obj has key that equals argument playerName.
+    // If true return playerName.points.
+    // Else look at key away.
+    // Look at key players.
+    // Check if players obj has key that equals argument playerName.
 
-//     // const playerPoints = game.away.players[playerName].points
-//     // console.log(playerPoints)
-//     for (let topLevelKey in game) {
-//         console.log(game[topLevelKey])
-//         let secondLevel = game[topLevelKey]
-//         for (let secondLevelKey in secondLevel.players) {
-//             // console.log(secondLevel[secondLevelKey])
-//             // console.log(secondLevel["players"])
-//             console.log(secondLevelKey)
-//             let getPlayer = secondLevel.players
-//             if (secondLevelKey === playerName) {
-//                 let playerPoints = getPlayer[secondLevelKey].points
-//                 console.log("THIS IS THE PLAYER: "+ secondLevelKey)
-//                 console.log(playerName+"'s POINTS: "+ playerPoints)
-//                 return playerPoints
-//             }
-//         }
-//     }
-// }
+    // const playerPoints = game.away.players[playerName].points
+    // console.log(playerPoints)
+    for (let topLevelKey in game) {
+        // console.log(game[topLevelKey])
+        let secondLevel = game[topLevelKey]
+        for (let secondLevelKey in secondLevel.players) {
+            // console.log(secondLevel[secondLevelKey])
+            // console.log(secondLevel["players"])
+            // console.log(secondLevelKey)
+            let getPlayer = secondLevel.players
+            if (secondLevelKey === playerName) {
+                let playerPoints = getPlayer[secondLevelKey].points
+                console.log("THIS IS THE PLAYER: "+ secondLevelKey)
+                console.log(playerName+"'s POINTS: "+ playerPoints)
+                return playerPoints
+            }
+        }
+    }
+}
 /* TEST numPointsScored() */
+// numPointsScored("Alan Anderson")
+// numPointsScored("Reggie Evans")
+// numPointsScored("Brook Lopez")
+// numPointsScored("Mason Plumlee")
+// numPointsScored("Jason Terry")
+
+// numPointsScored("Jeff Adrien")
+// numPointsScored("Bismak Biyombo")
+// numPointsScored("DeSagna Diop")
 // numPointsScored("Ben Gordon")
+// numPointsScored("Brendan Haywood")
 
 
 /* #2 shoeSize */
-// function shoeSize(playerName) {
-//     debugger
-//     let game = gameObject()
+/* 
+input: player's name
+output: return shoe size for that player
+*/
+function shoeSize(playerName) {
+    // debugger
+    let game = gameObject()
 
-//     // Look at key home.
-//     // Look at key players.
-//     // Check if players obj has key that equals argument playerName.
-//     // If true return playerName.points.
-//     // Else look at key away.
-//     // Look at key players.
-//     // Check if players obj has key that equals argument playerName.
+    // Look at key home.
+    // Look at key players.
+    // Check if players obj has key that equals argument playerName.
+    // If true return playerName.points.
+    // Else look at key away.
+    // Look at key players.
+    // Check if players obj has key that equals argument playerName.
 
-//     // const playerShoe = game.away.players[playerName].shoe
-//     // console.log(playerShoe)
-//     for (let topLevelKey in game) {
-//         console.log(game[topLevelKey])
-//         let secondLevel = game[topLevelKey]
-//         for (let secondLevelKey in secondLevel.players) {
-//             // console.log(secondLevel[secondLevelKey])
-//             // console.log(secondLevel["players"])
-//             console.log(secondLevelKey)
-//             let getPlayer = secondLevel.players
-//             if (secondLevelKey === playerName) {
-//                 let playerShoe = getPlayer[secondLevelKey].shoe
-//                 console.log("THIS IS THE PLAYER: "+ secondLevelKey)
-//                 console.log(playerName+"'s SHOE SIZE: "+ playerShoe)
-//                 return playerShoe
-//             }
-//         }
-//     }    
-// }
+    // const playerShoe = game.away.players[playerName].shoe
+    // console.log(playerShoe)
+    for (let topLevelKey in game) {
+        // console.log(game[topLevelKey])
+        let secondLevel = game[topLevelKey]
+        for (let secondLevelKey in secondLevel.players) {
+            // console.log(secondLevel[secondLevelKey])
+            // console.log(secondLevel["players"])
+            // console.log(secondLevelKey)
+            let getPlayer = secondLevel.players
+            if (secondLevelKey === playerName) {
+                let playerShoe = getPlayer[secondLevelKey].shoe
+                console.log("THIS IS THE PLAYER: "+ secondLevelKey)
+                console.log(playerName+"'s SHOE SIZE: "+ playerShoe)
+                return playerShoe
+            }
+        }
+    }    
+}
 /* TEST shoeSize() */
+// shoeSize("Alan Anderson")
+// shoeSize("Reggie Evans")
 // shoeSize("Brook Lopez")
+// shoeSize("Mason Plumlee")
+// shoeSize("Jason Terry")
+
+// shoeSize("Jeff Adrien")
+// shoeSize("Bismak Biyombo")
+// shoeSize("DeSagna Diop")
+// shoeSize("Ben Gordon")
+// shoeSize("Brendan Haywood")
+
 
 
 /* #3 teamColors */
 /* 
 input: team name
-output: array of team's colors
+output: return array of team's colors
 */
-// function teamColors(paramTeamName) {
-//     // debugger
-//     let game = gameObject()
+function teamColors(paramTeamName) {
+    // debugger
+    let game = gameObject()
 
-//     // Look at key home/ away.
-//     // Look at key players.
-//     // Check if --
+    // Look at key home/ away.
+    // Look at key players.
+    // Check if --
 
-//     // const teamColors = game.away.colors
-//     // console.log(teamColors)
+    // const teamColors = game.away.colors
+    // console.log(teamColors)
 
-//     for (let topLevelKey in game) {
-//         console.log(game[topLevelKey])
-//         let secondLevel = game[topLevelKey]
-//         console.log(secondLevel)
-//         if (secondLevel.teamName === paramTeamName) {
-//             let arrTeamColors = secondLevel.colors
-//             // console.log("Found: "+secondLevel.teamName)
-//             // console.log("Array: "+arrTeamColors)
-//             return arrTeamColors
-//         }
-//     }    
-// }
+    for (let topLevelKey in game) {
+        // console.log(game[topLevelKey])
+        let secondLevel = game[topLevelKey]
+        // console.log(secondLevel)
+        if (secondLevel.teamName === paramTeamName) {
+            let arrTeamColors = secondLevel.colors
+            console.log("Found: "+secondLevel.teamName)
+            console.log("Array: "+arrTeamColors)
+            return arrTeamColors
+        }
+    }    
+}
 // /* TEST teamColors() */
+// teamColors("Brooklyn Nets")
 // teamColors("Charlotte Hornets")
-
 
 /* #4 teamNames */
 /* 
 input: none
-output: array of teams' names
+output: return array of teams' names
 */
-// function teamNames() {
-//     debugger
-//     let game = gameObject()
+function teamNames() {
+    // debugger
+    let game = gameObject()
 
-//     // Look at key home/ away.
-//     // Look at key players.
-//     // Check if --
+    // Look at key home/ away.
+    // Look at key players.
+    // Check if --
 
-//     // const teamColors = game.away.colors
-//     // console.log(teamColors)
-//     const arrTeamNames = []
+    // const teamColors = game.away.colors
+    // console.log(teamColors)
+    const arrTeamNames = []
 
-//     for (let topLevelKey in game) {
-//         // console.log(game[topLevelKey])
-//         let secondLevel = game[topLevelKey]
-//         // console.log(secondLevel)
-//         // console.log(secondLevel.teamName)
-//         /* 1ST VERSION ADD TEAM NAME TO ARRAY */
-//         // arrTeamNames[arrTeamNames.length] = secondLevel.teamName
-//         /* REFACTORED ADD TEAM NAME TO ARRAY */
-//         arrTeamNames.push(secondLevel.teamName)
-//         console.log(arrTeamNames)
-//     } 
-//     console.log(`Return: ${arrTeamNames}`)   
-//     return arrTeamNames
-// }
+    for (let topLevelKey in game) {
+        // console.log(game[topLevelKey])
+        let secondLevel = game[topLevelKey]
+        // console.log(secondLevel)
+        // console.log(secondLevel.teamName)
+        /* 1ST VERSION ADD TEAM NAME TO ARRAY */
+        // arrTeamNames[arrTeamNames.length] = secondLevel.teamName
+        /* REFACTORED ADD TEAM NAME TO ARRAY */
+        arrTeamNames.push(secondLevel.teamName)
+        // console.log(arrTeamNames)
+    } 
+    console.log(`Return: ${arrTeamNames}`)   
+    return arrTeamNames
+}
 // /* TEST teamNames()*/
 // teamNames()
+
 
 /* #5 playerNumbers */
 /* 
@@ -142,7 +172,7 @@ input: team name
 output: array of team jersey numbers
 */
 function playerNumbers(teamName) {
-    debugger
+    // debugger
     let game = gameObject()
 
     // Access gameObject() object and assign as game.
@@ -174,6 +204,7 @@ function playerNumbers(teamName) {
         }
 
     }
+    console.log("teamName: " + teamName)
     console.log("Returning: " + arrJerseyNumbers)
     return arrJerseyNumbers
 }
@@ -185,10 +216,10 @@ function playerNumbers(teamName) {
 /* #6 playerStats */
 /* 
 input: player's name
-output: object of player's stats
+output: return object of player's stats
 */
 function playerStats(playerName) {
-    debugger
+    // debugger
     let game = gameObject()
 
     // Access gameObject() object and assign as game.
@@ -202,12 +233,13 @@ function playerStats(playerName) {
     // console.log(playerBenGordon)
 
     for (let topLevelKey in game) {
-        console.log(game[topLevelKey])
+        // console.log(game[topLevelKey])
             let secondLevel = game[topLevelKey]
             for (let secondLevelKey in secondLevel.players) {
-                console.log(secondLevelKey)
+                // console.log(secondLevelKey)
                 if (secondLevelKey === playerName) {
                     let objPlayerStats = secondLevel.players[secondLevelKey]
+                    console.log(playerName)
                     console.log(objPlayerStats)
                     return objPlayerStats
                 }
@@ -215,16 +247,67 @@ function playerStats(playerName) {
     }
 }
 /* TEST playerStats() */
-// playerStats()
 // playerStats("Alan Anderson")
-playerStats("Jeff Adrien")
+// playerStats("Reggie Evans")
+// playerStats("Brook Lopez")
+// playerStats("Mason Plumlee")
+// playerStats("Jason Terry")
 
+// playerStats("Jeff Adrien")
+// playerStats("Bismak Biyombo")
+// playerStats("DeSagna Diop")
+// playerStats("Ben Gordon")
+// playerStats("Brendan Haywood")
 
 
 /* #7 bigShoeRebounds */
+/* 
+input: no parameters
+output: return number of rebounds of player with biggest shoe size
+*/
+function bigShoeRebounds() {
+    // debugger
+    let game = gameObject()
 
+    // Access gameObject() object and assign as game.
+    // Iterate through game object to get keys.
+    // Use game[topLevelKeys].players[playerNames].shoe
+    
+    // e.g.
+    // const playerShoe = game.home.players["Mason Plumlee"].shoe
+    // console.log("BIGGEST SHOE SIZE: "+playerShoe)
+    // const playerRebounds = game.home.players["Mason Plumlee"].rebounds
+    // console.log("REBOUNDS OF BIGGEST SHOE SIZE:"+playerRebounds)
+    
+    let biggestShoeSize = 0
+    let playerBiggestShoeSize
+    let playerRebounds
+    for (let topLevelKey in game) {
+        // console.log(game[topLevelKey].teamName)
+        let secondLevel = game[topLevelKey]
+        for (let secondLevelKey in secondLevel.players) {
+            // console.log(secondLevelKey)
+            let shoeSize = secondLevel.players[secondLevelKey].shoe
+            if (shoeSize > biggestShoeSize) {
+                biggestShoeSize = shoeSize
+                playerBiggestShoeSize = secondLevelKey
+                playerRebounds = secondLevel.players[playerBiggestShoeSize].rebounds
+                // console.log("shoeSize: "+shoeSize)
+                // console.log("player: "+playerBiggestShoeSize)
+                // console.log("rebounds: "+playerRebounds)
 
+            }    
+        }
+    }
+    console.log(`${playerBiggestShoeSize} has the biggest shoe size equaling ${biggestShoeSize} with ${playerRebounds} rebounds.`)
+    return playerRebounds
+}
+/* TEST bigShoeRebounds() */
+// bigShoeRebounds()
 
+//////////////////////////////////////////////////////////////////
+// OBJECT
+//////////////////////////////////////////////////////////////////
 function gameObject() {
     return {
         "home":{"teamName": "Brooklyn Nets",
